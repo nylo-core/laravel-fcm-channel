fb008e20fbaa99dc659aa58b0df7a37ab1758e4a
Anthony <ants52@aol.com>
2024-06-07 12:10:37 +0100

    v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [1.0.1] - 2024-06-07
+
+* Update web.php file
+
 ## [1.0.0] - 2024-06-06
 
 * Initial release
--- src/routes/web.php
+++ src/routes/web.php
@@ -4,5 +4,3 @@ use Illuminate\Support\Facades\Route;
 use Nylo\LaravelFCM\Http\Controllers\LaravelFcmController;
 
 Route::put('device', [LaravelFcmController::class, 'update'])->name('laravel_notify_fcm.update');
-
-Route::post('device', [LaravelFcmController::class, 'store'])->name('laravel_notify_fcm.store');
